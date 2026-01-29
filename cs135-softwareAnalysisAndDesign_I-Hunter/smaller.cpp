110cff2602fa5733fee88460486074fd6c1c275f
Carey Jiang <careyj2024@gmail.com>
2026-01-29 00:47:42 -0500

    added lab1

---
+++ cs135-softwareAnalysisAndDesign_I-Hunter/smaller.cpp
@@ -0,0 +1,28 @@
+/*
+Copyright (c) 2026 Carey Jiang
+
+Author:      Carey Jiang
+Created:     2026-1-29
+Updated:     2026-1-29
+Description: Reads two integers from the user and prints the smaller value.
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    int a,b;
+
+    cout << "Enter the first number: ";
+    cin >> a;
+    cout << "Enter the second number: ";
+    cin >> b;
+
+    cout << "The smaller of the two is " << ((a < b) ? a : b) << endl;
+
+
+    return 0;
+}
