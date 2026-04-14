abe30a21479751f1f9d84ac02b2a430df5a4b074
Ejshzgd <2020caji5602@gmail.com>
2026-04-14 15:34:42 -0400

    finished lab 13 - Cs135

--- cs135-softwareAnalysisAndDesign_I-Hunter/13_lab/recursion.cpp
+++ cs135-softwareAnalysisAndDesign_I-Hunter/13_lab/recursion.cpp
@@ -10,6 +10,11 @@ Description: Define a function: void printRange(int left, int right);
              Define a function: int sumRange(int left, int right);
                 -Computes the sum of all numbers in range left <= x <= right
                 -Using only recursion
+             Define a function: int sumArray(int *arr, int size);
+                 -Receives an array (as a pointer to its first element) and the size of the array
+                 -Return the sum of its elements
+             Define a function: bool isAlphanumeric(string s);
+                 -Returns true if all characters in the string are letters and digits
 
 */  
 
@@ -21,6 +26,8 @@ using namespace std;
 //--- Function prototype ---
 void printRange(int left, int right);
 int sumRange(int left, int right);
+int sumArray(int *arr, int size);
+bool isAlphanumeric(string s);
 
 
 int main(){
@@ -30,10 +37,20 @@ int main(){
 
     //--Task B
     int x = sumRange(1, 3);
-    cout << "This is " << x << endl;   // 6
+    cout << "\nThis is " << x << endl;   // 6
     
     int y = sumRange(-2, 10);
     cout << "That is " << y << endl;   // 52
+
+    
+    int arr[] = {12,17,-5,3,7,-15,27,5,12,-21};
+
+    int sum1 = sumArray(arr, 10); // Add all elements
+    cout << "Sum is " << sum1 << endl;  // Sum is 42
+    
+    int sum2 = sumArray(arr, 5); // Add up first five elements
+    cout << "Sum is " << sum2 << endl;  // Sum is 34
+
 
     return 0;
 }
@@ -53,8 +70,6 @@ void printRange(int left, int right){
 }
 
 
-
-
 int sumRange(int left, int right){
 
     if(left == right)
@@ -64,8 +79,35 @@ int sumRange(int left, int right){
 
     if(left < right)
     {
-        return left += sumRange(left++,right);
+        return left += sumRange(left+1,right);
     }
 
-    return left;
+    return 0;
+}
+
+
+int sumArrayInRange(int *arr, int left, int right, int total){
+    total += arr[left];
+
+    if(left == right)
+    {
+        return total;
+    }
+
+    if(left < right)
+    {
+        return sumArrayInRange(arr,left+1,right,total);
+    }
+
+    return 0;
+}
+
+
+int sumArray(int *arr, int size){
+    return sumArrayInRange(arr,0,size-1,0);
+}
+
+
+bool isAlphanumeric(string s){
+    
 }
